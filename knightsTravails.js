0d100accc29d27bfe0cd6a28810c410fa40c8211
Carmen Morata <cmoratagarcia@gmail.com>
2025-07-14 14:12:09 -0700

    Write initial knightMoves function and store possible move coordinates

---
+++ knightsTravails.js
@@ -0,0 +1,15 @@
+function knightMoves(start, end) {
+  const boardSize = 8;
+
+  // All 8 possible moves
+  const knightMoves = [
+    [1, 2],
+    [2, 1],
+    [2, -1],
+    [1, -2],
+    [-1, -2],
+    [-2, -1],
+    [-2, 1],
+    [-1, 2],
+  ];
+}
